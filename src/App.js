e02ee0715ef925982857e2fecf3e0729cd08cb71
王伟韬 <wangweitao@admaster.com.cn>
2017-07-26 14:11:07 +0800

    update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <Clock />   
-        {/* <Counter /> */}
+        <Counter /> 
       </div>
     )
   }
--- src/Counter.js
+++ src/Counter.js
@@ -1,22 +1,24 @@
 import React from 'react'
 import moment from 'moment'
 import './Counter.css'
-const birth = moment('1994-09-07 10:00:00')
 
 export class Counter extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      ...this.count()
-    }
+      birth: moment('1994-09-07 10:00:00'),
+    }    
+  }
+  componentWillMount () {
+    this.setState(...this.count())
   }
   count () {
     let now = moment()
     return {
-      days: now.diff(birth, 'days'),
-      hours: now.diff(birth, 'hours'),
-      minutes: now.diff(birth, 'minutes'),
-      seconds: now.diff(birth, 'seconds')
+      days: now.diff(this.state.birth, 'days'),
+      hours: now.diff(this.state.birth, 'hours'),
+      minutes: now.diff(this.state.birth, 'minutes'),
+      seconds: now.diff(this.state.birth, 'seconds')
     }
   }
   componentDidMount () {
@@ -26,16 +28,16 @@ export class Counter extends React.Component {
   }
   render () {
     const {...past} = this.state
-    return (
+    return past.days && past.hours && past.seconds ? 
       <div className='counter'>
-        {'来到世上已经'}
         <div className='box'>
-          <div className='cell'>{`${past.days.toLocaleString()}天`}</div>
-          <div className='cell'>{`${past.hours.toLocaleString()}小时`}</div>
-          <div className='cell'>{`${past.minutes.toLocaleString()}分钟`}</div>
-          <div className='cell'>{`${past.seconds.toLocaleString()}秒`}</div>
+          {'来到世上已经:'}
+          <div className='cell'><span>{`${past.days.toLocaleString()}`}</span>天</div>
+          <div className='cell'><span>{`${past.hours.toLocaleString()}`}</span>小时</div>
+          <div className='cell'><span>{`${past.minutes.toLocaleString()}`}</span>分钟</div>
+          <div className='cell'><span>{`${past.seconds.toLocaleString()}`}</span>秒</div>
         </div>
-      </div>
-    )
+      </div> :
+      <div></div>
   }
 }
